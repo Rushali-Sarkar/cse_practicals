a56831947ead82c3a6bc07fee8938a7090671a73
Rushali Sarkar <rushalisarkar2000@gmail.com>
2021-03-01 18:30:29 +0530

    corrected insertion sort

--- C/insertionsort.c
+++ C/insertionsort.c
@@ -4,7 +4,7 @@
 
 int* insertionsort(int* arr, int size) {
     for (int i = 0; i < size; i++) {
-        int j = i + 1;
+        int j = i;
         while (j > 0 && arr[j] < arr[j - 1]) {
             int temp = arr[j];
             arr[j] = arr[j - 1];
